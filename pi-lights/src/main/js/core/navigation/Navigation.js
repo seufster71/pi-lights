71b52be2ada0ae53531b7f33fc81a530370b9817
seufster <seufster@gmail.com>
2017-09-10 21:45:30 -0400

    test

--- pi-lights/src/main/js/core/navigation/Navigation.js
+++ pi-lights/src/main/js/core/navigation/Navigation.js
@@ -3,9 +3,9 @@ import NavbarHeader from './NavbarHeader.js'
 import NavbarMenu from './../../navigationViews/NavbarMenu.js'
 
 export default function Navigation(props) {
-  return (
 
-	<nav className="navbar navbar-inverse navbar-fixed-top">
+  return (
+	   <nav className="navbar navbar-inverse navbar-fixed-top">
       <div className="container">
         <NavbarHeader headerName={props.headerName}/>
         <NavbarMenu/>
