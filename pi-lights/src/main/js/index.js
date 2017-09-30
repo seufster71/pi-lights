6a6173747a868f913f9279e45973a8dec4deb209
seufster <seufster@gmail.com>
2017-09-30 15:45:41 -0400

    update path

--- pi-lights/src/main/js/index.js
+++ pi-lights/src/main/js/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import NavigationContainer from './core/navigation/NavigationContainer.js';
-import PiLightsContainer from './lightswitch/PiLightsContainer.js';
+import PiLightsContainer from './piSwitch/piLights/PiLightsContainer.js';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import Theme from './theme.css';
 
 class App extends React.Component {
-	
+
 	constructor() {
 	    super();
 	    this.state = {
